510cd108db51e7cdc477e70e48b7e2d84367293e
Dante <motiv8lab@gmail.com>
2021-04-18 15:12:05 -0400

    2nd

--- app/src/main/java/com/example/rutgerscafe/MainActivity.java
+++ app/src/main/java/com/example/rutgerscafe/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.rutgerscafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+//    Button button = (Button) findViewById(R.id.donutButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                
+            }
+        });
     }
+
+    public void clickedDonut(View view) {
+        System.out.println("self made orange");
+    }
+
+
+
 }
